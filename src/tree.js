05ba37368fa1ec2689238ca657db710b139e979a
zhpadvoiski <zh.padvoiski@gmail.com>
2021-09-22 16:06:34 -0500

    add func added

--- src/tree.js
+++ src/tree.js
@@ -7,3 +7,39 @@ function Node(value, left, right) {
   this.left = left === undefined ? null : left;
   this.right = right === undefined ? null : right;
 }
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    let node = this.root;
+    if (!node) {
+      node = new Node(value);
+      return;
+    }
+
+    return search(node);
+
+    function search(node) {
+      if (value < node.value) {
+        if (!node.left) {
+          node.left = new Node(value);
+          return;
+        } else {
+          return search(node.left);
+        }
+      } else if (value > node.value) {
+        if (!node.right) {
+          node.right = new Node(value);
+          return;
+        } else {
+          return search(node.right);
+        }
+      } else {
+        return;
+      }
+    }
+  }
+}
