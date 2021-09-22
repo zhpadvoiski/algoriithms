dc2abd928da5c3920bd3a42d3611267a8f0923c3
zhpadvoiski <zh.padvoiski@gmail.com>
2021-09-22 16:26:18 -0500

    print

--- src/tree.js
+++ src/tree.js
@@ -2,7 +2,6 @@ function Node(value, left, right) {
   if (!value) {
     throw "value not initialized";
   }
-
   this.value = value === undefined ? null : value;
   this.left = left === undefined ? null : left;
   this.right = right === undefined ? null : right;
@@ -16,7 +15,7 @@ class Tree {
   add(value) {
     let node = this.root;
     if (!node) {
-      node = new Node(value);
+      this.root = new Node(value);
       return;
     }
 
@@ -42,4 +41,40 @@ class Tree {
       }
     }
   }
+  print() {
+    let current = this.root;
+    const values = [];
+
+    traverse(current);
+    console.log(values.join(", "));
+
+    function traverse(node) {
+      if (!node) {
+        return;
+      }
+      traverse(node.left);
+      values.push(node.value);
+      traverse(node.right);
+    }
+  }
+}
+
+main();
+
+function main() {
+  const tree = new Tree();
+  tree.add(10);
+  tree.add(5);
+  tree.add(9);
+  tree.add(12);
+  tree.add(27);
+  tree.add(2);
+  tree.add(7);
+  tree.add(28);
+  tree.add(18);
+  tree.add(8);
+  tree.add(4);
+  tree.add(13);
+  tree.print();
+  //   console.log(tree);
 }
